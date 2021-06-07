956240adafa739e7e1b82fe53398966b1b0a77fd
rvencu <vencu.richard@gmail.com>
2021-06-08 00:26:17 +0300

    recover from crashes tentative

--- system/crawl.sh
+++ system/crawl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /home/crawl/headless-crawlingathome/droplettest.py >> /home/crawl/crawl.log 2>&1
+while true; do python3 ~/headless-crawlingathome/droplettest.py >> ~/crawl.log 2>&1 && echo >> ~/crawl.log 2>&1 && echo "----- recovering from unexpected crash -----" >> ~/crawl.log 2>&1 && sleep 5 ;done
